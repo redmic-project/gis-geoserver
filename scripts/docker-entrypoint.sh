a6d8ee17c6ffbb46e3555e801ddaef5837ad67fd
Pedro Trujillo <pedro@redmic.es>
2018-08-13 10:55:00 +0100

    Separa geoserver en servicios y actualiza detalles
    Ahora cuenta con un servicio de maestro-esclavo y otro de esclavo, con
    distinto nivel de replicación.

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -4,42 +4,18 @@ set -e
 
 FILENAME="cluster"
 
-if [ -z "$(ls -A ${GEOSERVER_DATA_DIR})" ]; then
+if [ -z "$(ls -A ${GEOSERVER_DATA_DIR})" ]
+then
 	echo "Copy data_dir"
-	cp -r ${GEOSERVER_HOME}/data_dir/* ${GEOSERVER_DATA_DIR}
+	cp -r "${GEOSERVER_HOME}/data_dir/*" "${GEOSERVER_DATA_DIR}"
 fi
 
-if [ -z ${BROKER_SERVICE_NAME} ]; then
-    >&2 echo "Environment variable BROKER_SERVICE_NAME unset. You MUST set it to a service name."
-    exit 3
-fi
-
-if [ -n "${SWARM_MODE}" ]; then
-	SERVICE_PREFIX="tasks."
-else
-	SERVICE_PREFIX=""
-fi
-
-echo "Discovering service nodes..."
-# Docker swarm's DNS resolves special hostname "tasks.<service_name" to IP addresses of all containers inside overlay network
-SERVICE_NODES=$(dig ${SERVICE_PREFIX}${BROKER_SERVICE_NAME} +short)
-echo "Nodes of service ${BROKER_SERVICE_NAME}:"
-echo "${SERVICE_NODES}"
-
-NODE_IPS=""
-for NODE_IP in ${SERVICE_NODES}; do
-	if [ -n "${NODE_IPS}" ]; then
-		NODE_IPS="${NODE_IPS},"
-	fi
-	NODE_IPS="${NODE_IPS}tcp\://${NODE_IP}\:${ACTIVEMQ_PORT}"
-done
-
-export BROKER_URL="failover\:(${NODE_IPS})?jms.useAsyncSend\=true"
-
-if [ ${ROLE} == "master-slave" ]; then
+if [ "${ROLE}" == "master-slave" ]
+then
 	export SLAVE_ACTIVE="true"
 	export MASTER_ACTIVE="true"
-elif [ ${ROLE} == "master" ]; then
+elif [ "${ROLE}" == "master" ]
+then
 	export SLAVE_ACTIVE="false"
 	export MASTER_ACTIVE="true"
 else
@@ -51,7 +27,8 @@ fi
 
 envsubst < /${FILENAME}.template > ${CLUSTER_CONFIG_DIR}/${FILENAME}.properties
 
-if [ -n "${DEBUG}"]; then
+if [ -n "${DEBUG}"]
+then
 	cat ${CLUSTER_CONFIG_DIR}/${FILENAME}.properties
 fi
 
