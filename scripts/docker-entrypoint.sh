5b12fcb556f9a8c68aeae6b02e1d9f20017d9b1f
Pedro Trujillo <pedro@redmic.es>
2018-09-24 08:51:04 +0100

    Instala dig y lo usa para descubrir otros nodos

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -5,28 +5,38 @@ set -e
 if [ -z "$(ls -A ${GEOSERVER_DATA_DIR})" ]
 then
 	echo "Data dir is empty, creating data folders structure"
-    mkdir -p ${GEOSERVER_DATA_DIR}/coverages
-    mkdir -p ${GEOSERVER_DATA_DIR}/data
-    mkdir -p ${GEOSERVER_DATA_DIR}/gwc-layers
-    mkdir -p ${GEOSERVER_DATA_DIR}/layergroups
-    mkdir -p ${GEOSERVER_DATA_DIR}/workspaces
+	mkdir -p ${GEOSERVER_DATA_DIR}/coverages ${GEOSERVER_DATA_DIR}/data ${GEOSERVER_DATA_DIR}/gwc-layers \
+		${GEOSERVER_DATA_DIR}/layergroups ${GEOSERVER_DATA_DIR}/workspaces
 
-    cp -arv "${GEOSERVER_HOME}/data_dir/"* "${GEOSERVER_DATA_DIR}"
+	cp -arv "${GEOSERVER_HOME}/data_dir/"* "${GEOSERVER_DATA_DIR}"
 
   	echo "Delete data_dir in container"
 	rm -rf "${GEOSERVER_HOME}/data_dir"
 fi
 
-if [ ! -d "${GEOSERVER_DATA_DIR}" ]
-then
-    mkdir -p ${GEOSERVER_DATA_DIR}/cluster
+echo "Discovering other nodes in cluster..."
+serviceNodesIps=$(dig ${CLUSTER_DISCOVERY_URL} +short)
+echo "${serviceNodesIps}"
 
-    clusterTemplateFilename="cluster"
-    hazelcastTemplateFilename="hazelcast"
+myIp=$(dig ${HOSTNAME} +short)
+echo "My IP: ${myIp}"
 
-    envsubst < /${clusterTemplateFilename}.template > ${GEOSERVER_DATA_DIR}/cluster/${clusterTemplateFilename}.properties
-    envsubst < /${hazelcastTemplateFilename}.template > ${GEOSERVER_DATA_DIR}/cluster/${hazelcastTemplateFilename}.properties
-fi
+for nodeIp in ${serviceNodesIps}
+do
+	if [ "${nodeIp}" == "${myIp}" ];then
+		continue;
+	fi
+	clusterNodesIps="${clusterNodesIps}<hostname>${nodeIp}</hostname>"
+done
+export CLUSTER_NODES_IPS_TAGS="${clusterNodesIps}"
+
+mkdir -p ${GEOSERVER_DATA_DIR}/cluster
+
+clusterTemplateName="cluster"
+hazelcastTemplateName="hazelcast"
+
+envsubst < /${clusterTemplateName}.template > ${GEOSERVER_DATA_DIR}/cluster/${clusterTemplateName}.properties
+envsubst < /${hazelcastTemplateName}.template > ${GEOSERVER_DATA_DIR}/cluster/${hazelcastTemplateName}.properties
 
 export JAVA_OPTS="${JAVA_OPTS} ${GEOSERVER_OPTS} -DGEOSERVER_LOG_LOCATION=${GEOSERVER_LOG_LOCATION}"
 
