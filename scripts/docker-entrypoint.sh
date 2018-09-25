908f13ffcb02b0aaa49786f9b3b8948c62a36783
Pedro Trujillo <pedro@redmic.es>
2018-09-25 10:58:57 +0100

    Reunifica entrypoint y cambia lectura de ip

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -12,6 +12,34 @@ then
 	rm -rf "${GEOSERVER_HOME}/data_dir"
 fi
 
-/discovery.sh & disown
+echo "Discovering other nodes in cluster..."
+serviceNodesIps=$(dig ${CLUSTER_DISCOVERY_URL} +short)
+echo "${serviceNodesIps}"
+
+myIp=$(hostname -I | awk '{print $1}')
+echo "My hostname: ${HOSTNAME}"
+echo "My IP: ${myIp}"
+export MY_IP="${myIp}"
+
+for nodeIp in ${serviceNodesIps}
+do
+	clusterNodesIps="${clusterNodesIps}<member>${nodeIp}</member>"
+done
+export CLUSTER_NODES_IPS_TAGS="${clusterNodesIps}"
+
+export HAZELCAST_OUTBOUND_PORTS_RANGE=$((${HAZELCAST_PORT} + 101))-$((${HAZELCAST_PORT} + 201))
+
+clusterDir="${GEOSERVER_DATA_DIR}/cluster-${myIp}"
+clusterTemplateName="cluster"
+hazelcastTemplateName="hazelcast"
+
+mkdir -p ${clusterDir}
+
+envsubst < /${clusterTemplateName}.template > ${clusterDir}/${clusterTemplateName}.properties
+envsubst < /${hazelcastTemplateName}.template > ${clusterDir}/${hazelcastTemplateName}.xml
+
+export JAVA_OPTS="${JAVA_OPTS} ${GEOSERVER_OPTS} \
+	-DGEOSERVER_LOG_LOCATION=${GEOSERVER_LOG_LOCATION} \
+	-Dhazelcast.config.dir=${clusterDir}"
 
 exec "$@"
